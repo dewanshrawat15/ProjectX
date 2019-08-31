6e9bac7231744ed157d4c8ed1c74861a77c6aad1
Dewansh Rawat <dewanshrawat15@gmail.com>
2019-09-01 01:06:59 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,16 +1,71 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 
-browser = webdriver.Chrome(options=options,executable_path='/Users/dewanshrawat/Desktop/selenium/chromedriver')#add your chrome path here
-hastag = 'devdays191117'
-url = 'https://twitter.com/hashtag/'
-link = url + hastag
-browser.get(link)
+def call():
+	users_ref = db.collection('participants')
+	docs = users_ref.stream()
+	for doc in docs:
+		db.collection('participants').document(doc.id).delete()
 
-tweets = browser.find_elements_by_class_name('content')
-for i in range(0,len(tweets)):
-	print(tweets[i].text)
+	browser = webdriver.Chrome(executable_path='/Users/dewanshrawat/Desktop/selenium/chromedriver')#add your chrome path here
+	hastag = 'devdays0011'
+	url = 'https://twitter.com/hashtag/'
+	link = url + hastag
+	browser.get(link)
+
+	tweets = browser.find_elements_by_class_name('content')
+	val = []
+	data = []
+	for i in range(0,len(tweets)):
+		c = tweets[i].text
+		c = c.replace('\n', ' ')
+		a = c.split(" ")
+		for j in a:
+			data.append(j)
+
+	# print(data)
+
+	li = []
+	dict = {}
+	for i in range(len(data)):
+		c = data[i]
+		if '@' in c:
+			username = c
+			time = data[i+1][0]
+		elif 'Like' in c:
+			if data[i+1].isdigit():
+				likes = data[i+1]
+			else:
+				likes = 0
+		else:
+			continue
+		try:
+			if likes == -1:
+				continue
+			dict = {'username':username , 'likes':likes, 'time':time}
+			likes = -1
+			li.append(dict)
+		except:
+			continue
+
+	# print(li)
+
+
+	doc_ref = db.collection('participants')
+	for i in li:
+		doc_ref.add(i)
+
+	browser.close()
+
+
+while True:
+	call()
+	time.sleep(15)
